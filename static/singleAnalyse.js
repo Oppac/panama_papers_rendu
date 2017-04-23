7baf06e94f323e7091b1eec932058609ba78cae2
Oppac <oppac26@gmail.com>
2017-04-23 21:45:04 +0200

    Add files via upload

---
+++ static/singleAnalyse.js
@@ -0,0 +1,77 @@
+var analyse_cerlce_1 = [
+{ y: 6, indexLabel: "United Kingdom"},
+{ y: 2, indexLabel: "Canada"},
+{ y: 3, indexLabel: "Congo"},
+{ y: 6, indexLabel: "United Arab Emirates"},
+];
+var analyse_bar_1_1 = [
+{ y: 6, label: "United Kingdom"},
+{ y: 6, label: "United Arab Emirates"},
+{ y: 3, label: "Congo"},
+{ y: 2, label: "Canada"},
+];
+var analyse_bar_1_2 = [
+{ y: 3, label: "United Kingdom"},
+{ y: 0, label: "United Arab Emirates"},
+{ y: 0, label: "Congo"},
+{ y: 0, label: "Canada"},
+];
+var analyse_cerlce_2 = [
+{ y: 1, indexLabel: "Andorra"},
+{ y: 1, indexLabel: "Morocco"},
+{ y: 2, indexLabel: "Monaco"},
+{ y: 2, indexLabel: "United States of America"},
+{ y: 3, indexLabel: "Spain"},
+{ y: 1, indexLabel: "Luxembourg"},
+{ y: 2, indexLabel: "China"},
+{ y: 3, indexLabel: "Isle of Man"},
+{ y: 22, indexLabel: "Netherlands"},
+{ y: 12, indexLabel: "Portugal"},
+{ y: 3, indexLabel: "United Kingdom"},
+{ y: 2, indexLabel: "Virgin Islands UK"},
+{ y: 1, indexLabel: "Bahamas"},
+{ y: 4, indexLabel: "Brazil"},
+{ y: 25, indexLabel: "Switzerland"},
+];
+var analyse_bar_2_1 = [
+{ y: 25, label: "Switzerland"},
+{ y: 22, label: "Netherlands"},
+{ y: 12, label: "Portugal"},
+{ y: 4, label: "Brazil"},
+{ y: 3, label: "Spain"},
+{ y: 3, label: "Isle of Man"},
+{ y: 3, label: "United Kingdom"},
+{ y: 2, label: "Monaco"},
+{ y: 2, label: "China"},
+{ y: 2, label: "Virgin Islands UK"},
+{ y: 2, label: "United States of America"},
+{ y: 1, label: "Luxembourg"},
+{ y: 1, label: "Bahamas"},
+{ y: 1, label: "Morocco"},
+{ y: 1, label: "Andorra"},
+];
+var analyse_bar_2_2 = [
+{ y: 0, label: "Switzerland"},
+{ y: 0, label: "Netherlands"},
+{ y: 0, label: "Portugal"},
+{ y: 0, label: "Brazil"},
+{ y: 0, label: "Spain"},
+{ y: 0, label: "Isle of Man"},
+{ y: 6, label: "United Kingdom"},
+{ y: 0, label: "Monaco"},
+{ y: 0, label: "China"},
+{ y: 0, label: "Virgin Islands UK"},
+{ y: 0, label: "United States of America"},
+{ y: 0, label: "Luxembourg"},
+{ y: 0, label: "Bahamas"},
+{ y: 0, label: "Morocco"},
+{ y: 0, label: "Andorra"},
+];
+var analyse_cerlce_3 = [
+{ y: 37, indexLabel: "Total des realtions internes"},
+{ y: 17, indexLabel: "Total des relations entrantes"},
+];
+var analyse_cerlce_4 = [
+{ y: 37, indexLabel: "Total des realtions internes"},
+{ y: 84, indexLabel: "Total des relations sortante"},
+];
