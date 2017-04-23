96cb11fcd49fc50fd5168fc1dd7c21c695753c4b
Oppac <oppac26@gmail.com>
2017-04-23 21:41:42 +0200

    Update countries_relation.py

--- countries_relation.py
+++ countries_relation.py
@@ -15,7 +15,7 @@ t = time.time()
 driver = GraphDatabase.driver("bolt://localhost", auth=basic_auth("neo4j", "Alien"))
 session = driver.session()
 
-query = "MATCH (n)-[r]->(m) where NOT r.rel_type= \"registered_address\" AND NOT r.rel_type =~ \"same*\" and NOT n.country_codes = m.country_codes WITH [n.country_codes, m.country_codes] as list return list"
+query = "MATCH (n)-[r]->(m) where NOT r.rel_type= \"registered_address\" AND NOT r.rel_type =~ \"same*\" AND NOT r.rel_type =~ \"similare*\" WITH [n.country_codes, m.country_codes] as list return list"
 table_inter = session.run(query);
 
 couple = [" ", " "]
@@ -52,6 +52,9 @@ for record in nb_entities:
     for i in record["list"]:
         code = str(i)
         for src in code.split(';'):
+            if src not in country_id:
+                country_id[src] = count
+                count += 1
             if src not in country_value:
                 country_value[src] = 1
             else:
@@ -59,53 +62,14 @@ for record in nb_entities:
 
 session.close()
 
+with open('utils/country_relation.json', 'w') as outfile:
+    json.dump(country_relation, outfile)
 
-t = time.time()-t
-print t
+with open('utils/country_id.json', 'w') as outfile:
+    json.dump(country_id, outfile)
 
-
-def rotate(origin, point, angle):
-    """
-    Rotate a point counterclockwise by a given angle around a given origin.
-
-    The angle should be given in radians.
-    """
-    ox, oy = origin
-    px, py = point
-
-    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
-    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
-    return qx, qy
-
-def write_node(ouput_file, node_id, node_label, node_title, node_value, node_group, x, y):
-    line = "{id: "+str(node_id)+",  label: '"+str(node_label)+"',  title: \""+str(node_title)+"\",  value: "+str(node_value)+", group: "+str(node_group)+", x: "+str(x)+", y: "+str(y)+", },\n"
-    ouput_file.write(line)
-
-def write_edge(ouput_file, edge_from, edge_to, edge_value):
-    line = "{from: "+str(edge_from)+",  to: "+str(edge_to)+",  value: '"+str(edge_value)+"' },\n"
-    ouput_file.write(line)
-
-t = time.time()
-
-with open('AlphaCode.json') as country_coordinates_file:
-	country_coordinates = json.load(country_coordinates_file)
-
-ouput_file = open('countries_relation.js', 'w')
-ouput_file.write("var nodes = [\n")
-for src in country_id:
-    x, y = rotate((0,0), (country_coordinates[src][0],country_coordinates[src][1]), math.radians(-90))
-    value = 0
-    if src in country_value:
-        value = country_value[src]
-    write_node(ouput_file, country_id[src], src, src, math.sqrt(value)/10, 1, x*1000, y*1000)
-ouput_file.write("];\n")
-
-ouput_file.write("var edges = [\n")
-for src in country_relation:
-    for dst in country_relation[src]:
-        write_edge(ouput_file, country_id[src], country_id[dst], 2)
-ouput_file.write("];\n")
-
+with open('utils/country_value.json', 'w') as outfile:
+    json.dump(country_value, outfile)
 
 t = time.time()-t
 print t
